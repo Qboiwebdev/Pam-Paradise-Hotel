cf6cb226de405e76783d256dd8a8a071cfe49b71
Qboiwebdev <fataiqoweeypamilerin@gmail.com>
2026-08-06 01:03:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 const menuBtn = document.getElementById("menuBtn");
-
 const navbar = document.getElementById("navbar");
 
 menuBtn.addEventListener("click", () => {
-
   navbar.classList.toggle("active");
-
   if (navbar.classList.contains("active")) {
     menuBtn.innerHTML = "⨉";
   } else {
@@ -14,146 +11,179 @@ menuBtn.addEventListener("click", () => {
 });
 
 function getNights(i) {
-
   const start = new Date(document.getElementById("checkin" + i).value);
-
   const end = new Date(document.getElementById("checkout" + i).value);
-
   const diff = end - start;
-
   const nights = diff / (1000 * 60 * 60 * 24);
-
   return nights > 0 ? nights : 1;
 
 }
 
-
 function resetRoom(i) {
-
   document.getElementById("name" + i).value = "";
-
   document.getElementById("email" + i).value = "";
-
   document.getElementById("checkin" + i).value = "";
-
   document.getElementById("checkout" + i).value = "";
-
   document.getElementById("guests" + i).value = "";
-
 }
 
-
 function goToDefault(i) {
-
   resetRoom(i);
-
   window.location.hash = "ff" + i;
-
 }
 
 
 function payWithPaystack(pricePerNight, i) {
-
   const bookingCode = 'BK' + Math.floor(Math.random() * 1000000);
-
   const name = document.getElementById("name" + i).value.trim();
-
   const email = document.getElementById("email" + i).value.trim();
-
   const checkin = document.getElementById("checkin" + i).value;
-
   const checkout = document.getElementById("checkout" + i).value;
-
   const guests = document.getElementById("guests" + i).value;
 
-
-
   if (!name || !email || !checkin || !checkout || !guests) {
-
     alert("Please fill all the form ❗");
-
     return;
-
   }
 
-
   if (guests < 1 || guests > 10) {
-
     alert("Guests must be between 1 and 10");
-
     return;
-
   }
 
   const nights = getNights(i);
-
   const total = pricePerNight * nights;
-
   let handler = PaystackPop.setup({
-
     key: "pk_live_7849f7de72347120ae622c4ec281047832232044",
-
     email: email,
-
     amount: total * 100,
-
     currency: "NGN",
-
     metadata: {
-
-      custom_fields: [
-
-        { display_name: "Name", value: name },
-
-        { display_name: "Guests", value: guests },
-
-        { display_name: "Check-in", value: checkin },
-
-        { display_name: "Check-out", value: checkout },
-
-        { display_name: "Nights", value: nights },
-
-        { display_name: "Total", value: total },
-
-        { display_name: "Booking Code", value: bookingCode }
-
-      ]
-
+        custom_fields: [
+            { display_name: "Name", value: name },
+            { display_name: "Guests", value: guests },
+            { display_name: "Check-in", value: checkin },
+            { display_name: "Check-out", value: checkout },
+            { display_name: "Nights", value: nights },
+            { display_name: "Total", value: total },
+            { display_name: "Booking Code", value: bookingCode }
+        ]
     },
-
 
     callback: function (response) {
-
-      alert(
-
-        "Payment Successful ✔️\nBooking Confirmed ✔️\nBooking Code: " + bookingCode
-
-      );
-
-      goToDefault(i);
-
+        const customerParams = {
+            subject: "Booking Confirmation - Pam Paradise Hotel",
+            customer_email: email,
+            customer_name: name,
+            message: "Your booking has been confirmed.",
+            booking_details: 
+            `Booking Code:${bookingCode}
+            Check-in:${checkin}
+            Check-out:${checkout}
+            Guests:${guests}
+            Number of Nights:${nights}
+            Amount Paid:₦${total}
+            Payment Reference:${response.reference}`,
+            footer_message: "Please keep your booking code safe."
+        };
+        
+        const ownerParams = {
+            subject: "New Booking - Pam Paradise Hotel",
+            customer_name: name,
+            customer_email: email,
+            message: "A new booking has been received.",
+            booking_details: 
+            `Booking Code:${bookingCode}
+            Check-in:${checkin}
+            Check-out:${checkout}
+            Guests:${guests}
+            Number of Nights:${nights}
+            Amount:₦${total}
+            Payment Reference:${response.reference}`,
+            footer_message: ""
+        };
+        
+        Promise.all([
+            emailjs.send(
+                "service_t2jayrh",
+                "template_kqsw70w",
+                customerParams
+            ),
+            
+            emailjs.send(
+                "service_t2jayrh",
+                "template_6xbznyv",
+                ownerParams
+            )
+        ])
+        
+        .then(() => {
+            alert(
+                "Payment Successful ✔️\nBooking Confirmed ✔️\nBooking Code: " +
+                bookingCode
+            );
+            goToDefault(i);
+        })
+        
+        .catch((error) => {
+            console.log(error);
+            alert("Payment successful but email failed.");
+            goToDefault(i);
+        });
     },
-
-
+    
     onClose: function () {
-
-      alert("Booking not confirmed ❌");
-
-      goToDefault(i);
-
+        alert("Booking not confirmed ❌");
+        goToDefault(i);
     }
+});
+handler.openIframe();
+}
 
-  });
+function sendRefundEmail() {
+    const customerParams = {
+        subject: "Refund Successful - Pam Paradise Hotel",
+        customer_email: email,
+        customer_name: name,
+        message: "Your refund has been successfully processed.",
+        booking_details: 
+        `Booking Code:${bookingCode}
+        Refund Amount:₦${total}
+        Payment Reference:${paymentReference}`,
+        footer_message: "The refund has been returned to your payment account."
+    };
 
-  handler.openIframe();
+    const ownerParams = {
+        subject: "Refund Processed - Pam Paradise Hotel",
+        customer_name: name,
+        customer_email: email,
+        message: "A refund has been processed.",
+        booking_details: 
+        `Booking Code:${bookingCode}
+        Refund Amount:₦${total}
+        Payment Reference:${paymentReference}`,
+        footer_message: "Refund was successfully returned to the customer."
+    };
 
+    Promise.all([
+        emailjs.send(
+            "service_t2jayrh",
+            "template_kqsw70w",
+            customerParams
+        ),
+
+        emailjs.send(
+            "service_t2jayrh",
+            "template_6xbznyv",
+            ownerParams
+        )
+    ]);
 }
 
 const phone = "2348165803719";
 const msg =
   "Hello 🏨 \n\nI'd like to book/inquire:\n1. Room type: \n2. Check-in date: \n3. Check-out date: \n4. Nights: \n5. No. of guests: \n\nThank you!";
 document.getElementById("whatsapp-btn").href =
-
   "https://wa.me/" + phone + "?text=" + encodeURIComponent(msg);
 
 
@@ -169,15 +199,11 @@ const suggestions = document.getElementById("suggestions");
 let noResultDiv = null;
 
 searchBtn.addEventListener("click", () => {
-
     searchContainer.classList.add("active");
     searchBtn.style.display = "none"
-
 });
 
-
 closeBtn.addEventListener("click", () => {
-
     searchContainer.classList.remove("active");
     searchBtn.style.display = "block"
 
@@ -185,7 +211,6 @@ closeBtn.addEventListener("click", () => {
     rooms.forEach(room => {
         room.style.display = "";
     });
-
     suggestions.innerHTML = "";
     suggestions.style.display = "none";
 
@@ -205,21 +230,15 @@ closeBtn.addEventListener("click", () => {
 suggestions.style.display = "none";
 
 searchInput.addEventListener("input", function () {
-
     const value = this.value.toLowerCase().trim();
-
     let found = false;
-
     suggestions.innerHTML = "";
 
     if (value === "") {
-
         rooms.forEach(room => {
             room.style.display = "";
         });
-
         suggestions.style.display = "none";
-
         if (noResultDiv) {
             noResultDiv.remove();
             noResultDiv = null;
@@ -229,9 +248,7 @@ searchInput.addEventListener("input", function () {
         return;
     }
 
-    // FILTER ROOMS
     rooms.forEach(room => {
-
         const title = room.querySelector("h3")
             .textContent
             .trim()
@@ -251,50 +268,36 @@ searchInput.addEventListener("input", function () {
             found = true;
 
             const item = document.createElement("div");
-
             item.classList.add("suggestion-item");
             item.textContent = room.querySelector("h3").textContent.trim();
 
             item.addEventListener("click", () => {
-
                 const selectedTitle = item.textContent;
-
                 searchInput.value = selectedTitle;
-
                 suggestions.innerHTML = "";
                 suggestions.style.display = "none";
 
                 rooms.forEach(r => {
-
                     const roomTitle = r.querySelector("h3")
                         .textContent
                         .trim();
 
                     if (roomTitle === selectedTitle) {
-
                         r.style.display = "";
-
                         r.scrollIntoView({
                             behavior: "smooth",
                             block: "start"
                         });
-
                     } else {
-
                         r.style.display = "none";
-
                     }
                 });
 
                 AOS.refresh();
             });
-
             suggestions.appendChild(item);
-
         } else {
-
             room.style.display = "none";
-
         }
     });
 
